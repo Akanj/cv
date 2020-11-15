01d39925804f96d5c902fc566d004b6801354d90
Angelina Aïtkaci-Ali <angelina.aitkaciali@api-and-you.com>
2020-11-15 22:10:17 +0100

    [EDIT] Design général OK

--- cv/src/App.vue
+++ cv/src/App.vue
@@ -1,54 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
+    <v-main itemscope itemtype="http://schema.org/Person">
       <header>
-        <h1>Angelina Aïtkaci-Ali</h1>
+        <h1 itemprop="name"><span itemprop="givenName">Angelina</span> <span itemprop="familyName">Aïtkaci-Ali</span></h1>
         <h2>Développeuse Front-end</h2>
       </header>
+
       <Objectives/>
 
-      <Experience/>gi
+      <Experience/>
 
-      <Skills/>
-
-      <Training/>
+      <section>
+        <Skills/>
+        <Training/>
+      </section>
 
       <Contact/>
 
@@ -81,3 +46,59 @@
     }),
   });
 </script>
+
+<style lang="scss">
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  ::-moz-selection { /* Code for Firefox */
+    color: white;
+    background: #3DD2CC;
+  }
+  ::selection {
+    color: white;
+    background: #3DD2CC;
+  }
+  main {
+    margin: 0 15px 15px;
+  }
+  h1, h3, .theme--light.v-card .v-card__subtitle.subtitle {
+    color: #3DD2CC;
+    text-transform: uppercase;
+  }
+  h1, h2 {
+    text-transform: uppercase;
+    text-align: center;
+  }
+  h1 {
+    letter-spacing: 10px;
+    margin-bottom: 20px;
+  }
+  h2 {
+    letter-spacing: 6px;
+    margin-bottom: 15px;
+  }
+  h3 {
+    margin-bottom: 15px;
+  }
+  header {
+    margin: 15px 0 30px;
+    border-bottom: 3px solid #3DD2CC;
+  }
+  article {
+    margin-bottom: 15px;
+  }
+
+  h3 {
+    letter-spacing: 3px;
+  }
+  .theme--light.v-application {
+    h2, .title, p, li, a {
+      color: #3E6B89;
+    }
+  }
+  .mdi {
+    color: #3DD2CC;
+  }
+</style>
